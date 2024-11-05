a3eb398aaa5b2b2ae83c2a4d1b6b878491821708
Sochkov Ilya Semenovich <90711926+IlyaSochkov@users.noreply.github.com>
2024-11-05 16:54:57 +0300

    Delete frontend/src directory

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './src/router';
-
-const app = createApp(App); // Создание экземпляра приложения
-app.use(router); // Использование маршрутизатора
-app.mount('#app'); // Монтирование приложения в DOM
