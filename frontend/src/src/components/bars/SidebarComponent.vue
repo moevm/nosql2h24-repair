d411302054379e2a8a5f9fce2b8573bfea7c3a1d
Sochkov Ilya Semenovich <90711926+IlyaSochkov@users.noreply.github.com>
2024-11-11 14:42:05 +0300

    Update SidebarComponent.vue

--- frontend/src/src/components/bars/SidebarComponent.vue
+++ frontend/src/src/components/bars/SidebarComponent.vue
@@ -1,9 +1,5 @@
 <template>
     <aside class="sidebar">
-      <div v-if="isCustomer" class="menu-item" @click="goToStatistics">
-        <img src="@/assets/icons/statics.png" alt="Static Icon" class="static-icon">
-        <p>Статистика</p>
-      </div>
       <div class="menu-item" @click="logOut">
         <img src="@/assets/icons/logout.png" alt="Logout Icon" class="logout-icon">
       </div>
@@ -13,16 +9,7 @@
   <script>
   import axios from 'axios';
   export default {
-    data() {
-      return {
-        isCustomer: true, // Условие для отображения раздела статистики для заказчика
-      };
-    },
     methods: {
-      goToStatistics() {
-        // Логика перехода на страницу статистики
-        this.$router.push('/statistics');
-      },
       async logOut() {
         // Логика выхода на страницу авторизации
         try {
@@ -66,17 +53,6 @@
     flex-direction: column; /* Размещает элементы в столбец */
     align-items: center; /* Центрирует элементы по горизонтали */
     cursor: pointer;
-  }
-  
-  .static-icon {
-    width: 50px; /* Задайте нужный размер иконки */
-    height: 50px; /* Задайте нужный размер иконки */
-    margin-bottom: 10px; /* Отступ между иконкой и текстом */
-  }
-  
-  .menu-item p {
-    margin: 0; /* Убираем отступы у текста */
-    font-size: 16px; /* Задайте нужный размер текста */
   }
 
   .logout-item {
